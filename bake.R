b8630a3fe3827d86d5786aedab645f14e4f5a630
jennybc <jenny@stat.ubc.ca>
2013-10-09 21:14:11 -0700

    Refining block on custom qualitative colours in ggplot2

--- bake.R
+++ bake.R
@@ -7,7 +7,7 @@ herd(pub.dir = "/Users/jenny/web/fhWeb//publish//STAT545A", backend = "pandoc")
 
 herd(pub.dir = "/Users/jenny/web/fhWeb//publish//STAT545A")
 
-cull(cull.dir = "/Users/jenny/web/fhWeb//publish//STAT545A")
+#cull(cull.dir = "/Users/jenny/web/fhWeb//publish//STAT545A")
 
 knit2html("test.rmd")
 herd(yaml = "test.rmd")
